e75b56bbd1ee0d0b1263967f183f108d9c47c5d1
David <thedavidwatts@gmail.com>
2015-03-08 20:36:06 +0000

    Deleted note

--- includes/connect.php
+++ includes/connect.php
@@ -2,7 +2,7 @@
     define("DB_SERVER", "localhost");
     define("DB_USER", "YOUR USERNAME");
     define("DB_PASS", "YOUR DATABASE PASSWORD");
-    define("DB_NAME", "NAME OF DATABSE"); // when using dakar, this will be the same as your username
+    define("DB_NAME", "NAME OF DATABSE"); 
 
     $connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
 
