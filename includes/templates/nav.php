31bccd45eb80ec54bb184a200ccdd8be62b7468c
David <thedavidwatts@gmail.com>
2015-03-12 16:14:48 +0000

    Wiki link added

--- includes/templates/nav.php
+++ includes/templates/nav.php
@@ -3,7 +3,7 @@
         <a href="" class="on">Home</a>
         <a href="../public/aboutus.html">About Us</a>
         <a href="./help.html">Help</a>
-        <a href="">Wiki</a>
+        <a href="http://dakar.bournemouth.ac.uk/~ceagle/wiki/dokuwiki/doku.php?id=start">Wiki</a>
         <div class="login"> 
         <a href="../includes/login.html">Login</a>
         </div>
