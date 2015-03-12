d26dc5c630f7b1351260ac9d81b792c60d03d674
chrislangley666 <chrislangley666@hotmail.co.uk>
2015-03-12 14:48:08 +0000

    Error in Index (PHP)
    missing ‘;’

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
     session_start();
     require_once("../includes/connect.php");
     require_once("../includes/functions.php"); 
-    include_once("../includes/templates/header.php")
+    include_once("../includes/templates/header.php");
     include_once("../includes/templates/nav.php"); 
 
 ?>
