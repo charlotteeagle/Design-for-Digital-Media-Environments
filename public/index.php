a21ae5334b06858de0fee8e9d261d603e369bd46
David <thedavidwatts@gmail.com>
2015-03-12 20:17:07 +0000

    Adjusted Title

--- public/index.php
+++ public/index.php
@@ -11,7 +11,7 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title>BH Homes</title>
+    <title>Bournemouth Homes</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
     <link href='http://fonts.googleapis.com/css?family=Play' rel='stylesheet' type='text/css'>
   </head>
