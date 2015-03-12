b4a3f07c16b10568bd3254ee9ea6ef51e847c970
David <thedavidwatts@gmail.com>
2015-03-12 19:50:29 +0000

    Added links and includes

--- includes/templates/nav.php
+++ includes/templates/nav.php
@@ -1,11 +1,11 @@
 <div class="menu">
     <div class="buttons">
         <a href="" class="on">Home</a>
-        <a href="../public/aboutus.html">About Us</a>
-        <a href="./help.html">Help</a>
+        <a href="../../public/about.php">About Us</a>
+        <a href="../../public/help.php">Help</a>
         <a href="http://dakar.bournemouth.ac.uk/~ceagle/wiki/dokuwiki/doku.php?id=start">Wiki</a>
         <div class="login"> 
-        <a href="../includes/login.html">Login</a>
+        <a href="../../public/login.php">Login</a>
         </div>
         
     </div>
