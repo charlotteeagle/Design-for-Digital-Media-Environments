76afaf8ef9922619d8111e959dfe2f70dc510b0b
David <thedavidwatts@gmail.com>
2015-03-12 20:47:57 +0000

    added sign up page

---
+++ public/signup.php
@@ -0,0 +1,16 @@
+<?php 
+    session_start();
+    require_once("../includes/connect.php");
+    require_once("../includes/functions.php"); 
+    include_once("../includes/templates/header.php");
+    include_once("../includes/templates/nav.php"); 
+
+?>
+
+<body>
+
+
+</body>
+
+
+<?php include_once("../includes/templates/footer.php"); ?>   
