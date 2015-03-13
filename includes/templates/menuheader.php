e30d7d715cc48c807f18438dcecfa1a50b6304a3
chrislangley666 <chrislangley666@hotmail.co.uk>
2015-03-13 12:44:41 +0000

    Login button changes.

--- includes/templates/menuheader.php
+++ includes/templates/menuheader.php
@@ -11,13 +11,14 @@
     <div class="menu-header">   
         
             <div class="buttons">
-                <div class="login">  
-                    <a href="../../public/index.php" class="on">Home</a>
-                    <a href="../../public/about.php">About Us</a>
-                    <a href="../../public/help.php">Help</a>
-                    <a href="http://dakar.bournemouth.ac.uk/~ceagle/wiki/dokuwiki/doku.php?id=start">Wiki</a>
-                    <a href="../../public/login.php">Login</a>
-                </div>
+                <a href="../../public/index.php" class="on">Home</a>
+                <a href="../../public/about.php">About Us</a>
+                <a href="../../public/help.php">Help</a>
+                <a href="http://dakar.bournemouth.ac.uk/~ceagle/wiki/dokuwiki/doku.php?id=start">Wiki</a>
+                
+            <div class="login"> 
+                <a href="../../public/login.php">Login</a>
+            </div>
         
             </div>
      </div> 
