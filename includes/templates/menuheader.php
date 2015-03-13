e3803f49b759e8e8ce0506b147b7d38ce1bc289a
chrislangley666 <chrislangley666@hotmail.co.uk>
2015-03-13 12:41:01 +0000

    Put logo into button div.

--- includes/templates/menuheader.php
+++ includes/templates/menuheader.php
@@ -11,18 +11,15 @@
     <div class="menu-header">   
         
             <div class="buttons">
-            <a href="../../public/index.php" class="on">Home</a>
-            <a href="../../public/about.php">About Us</a>
-            <a href="../../public/help.php">Help</a>
-            <a href="http://dakar.bournemouth.ac.uk/~ceagle/wiki/dokuwiki/doku.php?id=start">Wiki</a>
-            <div class="login"> 
-            <a href="../../public/login.php">Login</a>
-            </div>
+                <div class="login">  
+                    <a href="../../public/index.php" class="on">Home</a>
+                    <a href="../../public/about.php">About Us</a>
+                    <a href="../../public/help.php">Help</a>
+                    <a href="http://dakar.bournemouth.ac.uk/~ceagle/wiki/dokuwiki/doku.php?id=start">Wiki</a>
+                    <a href="../../public/login.php">Login</a>
+                </div>
         
             </div>
-        
-
-
      </div> 
 </body>    
 
