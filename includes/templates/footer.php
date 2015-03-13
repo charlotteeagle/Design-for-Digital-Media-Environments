ec149176a6d70f6140a72fd8efdcf0cc84d0e87d
chrislangley666 <chrislangley666@hotmail.co.uk>
2015-03-13 16:31:42 +0000

    .

--- includes/templates/footer.php
+++ includes/templates/footer.php
@@ -10,11 +10,7 @@
                 <a href="https://twitter.com/?lang=en-gb"><img src="images/twitterlogo.png"></a>
             </div>
         </div>
-        <div class="sign-up">
-            <div class="signup-icon">
-                <a href="..//..//public/signup.php"><img src="images/Add%20User-50.png"></a>
-            </div> 
-        </div>
+    
         <div class="our-service">
             <div class="service-icon">
                 <img src="images/Info-50.png">
