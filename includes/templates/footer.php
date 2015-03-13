a8b4167fbad68d91b7b799073d8891fca5b08058
chrislangley666 <chrislangley666@hotmail.co.uk>
2015-03-13 22:10:32 +0000

    footer test

--- includes/templates/footer.php
+++ includes/templates/footer.php
@@ -3,7 +3,7 @@
 <body>
     <div class="footer">
         <div class="social-media">
-            <div class="social-icon">
+            <div class="icon-container">
                 <a href="https://en-gb.facebook.com/login"><img src="images/facebooklogo.png"></a>
                 <a href="https://twitter.com/?lang=en-gb"><img src="images/twitterlogo.png"></a>
                 <a href=""><img src="images/Info-50.png"></a>
