d67b7dfa6c18abba2fbddcb19cfbfd4c9cab263d
chrislangley666 <chrislangley666@hotmail.co.uk>
2015-03-13 11:42:43 +0000

    Removed nav and put it into the menu header.

--- includes/templates/menuheader.php
+++ includes/templates/menuheader.php
@@ -1,3 +1,5 @@
+
+
 <!doctype html>
 <html>
     <head>
@@ -8,7 +10,21 @@
 <!--Menu Header -->
  <body>
     <div class="menu-header">   
-    </div> 
+        <div class="menu">
+            <div class="buttons">
+            <a href="../../public/index.php" class="on">Home</a>
+            <a href="../../public/about.php">About Us</a>
+            <a href="../../public/help.php">Help</a>
+            <a href="http://dakar.bournemouth.ac.uk/~ceagle/wiki/dokuwiki/doku.php?id=start">Wiki</a>
+            <div class="login"> 
+            <a href="../../public/login.php">Login</a>
+            </div>
+        
+            </div>
+        </div>
+
+
+     </div> 
 </body>    
 
     
