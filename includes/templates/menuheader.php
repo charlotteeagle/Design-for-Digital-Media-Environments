9dc183d8e1e31b7ba4f7a1dcc352ecab9a509029
chrislangley666 <chrislangley666@hotmail.co.uk>
2015-03-13 11:58:56 +0000

    menu changes.

--- includes/templates/menuheader.php
+++ includes/templates/menuheader.php
@@ -9,20 +9,18 @@
 <!--Menu Header -->
  <body>
     <div class="menu-header">   
-        <div class="menu">
+        
             <div class="buttons">
             <a href="../../public/index.php" class="on">Home</a>
             <a href="../../public/about.php">About Us</a>
             <a href="../../public/help.php">Help</a>
             <a href="http://dakar.bournemouth.ac.uk/~ceagle/wiki/dokuwiki/doku.php?id=start">Wiki</a>
+            
             <div class="login"> 
             <a href="../../public/login.php">Login</a>
             </div>
         
             </div>
-        </div>
-
-
      </div> 
 </body>    
 
