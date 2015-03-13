e67492b7b72429e41e8334e282c8fea17f8e9482
chrislangley666 <chrislangley666@hotmail.co.uk>
2015-03-13 22:02:54 +0000

    Footer Test

--- includes/templates/footer.php
+++ includes/templates/footer.php
@@ -6,8 +6,9 @@
             <div class="social-icon">
                 <a href="https://en-gb.facebook.com/login"><img src="images/facebooklogo.png"></a>
                 <a href="https://twitter.com/?lang=en-gb"><img src="images/twitterlogo.png"></a>
-                <div class="service-icon">
-                    <a img src="images/Info-50.png"> </a>
+                <img src="images/Info-50.png">
+               <!-- <div class="service-icon"> -->
+                     
                 </div> 
             </div>
         </div>
