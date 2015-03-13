0af0577ce9d6bc6f6bdc2052f982e7fe37f71cfd
chrislangley666 <chrislangley666@hotmail.co.uk>
2015-03-13 21:34:11 +0000

    Added footer text.

--- includes/templates/footer.php
+++ includes/templates/footer.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html>
-    <body>
-      <div class="footer">
+<body>
+    <div class="footer">
         <div class="social-media">
             <div class="social-icon">
                 <a href="https://en-gb.facebook.com/login"><img src="images/facebooklogo.png"></a>
@@ -9,13 +9,12 @@
             <div class="social-icon">
                 <a href="https://twitter.com/?lang=en-gb"><img src="images/twitterlogo.png"></a>
             </div>
-        </div>
-    
-        <div class="our-service">
-            <div class="service-icon">
-                
-                <img src="images/Info-50.png">
-            </div> 
-        </div>
+            </div>
+            <div class="our-service">
+                <div class="service-icon">
+                    <img src="images/Info-50.png">
+                    <a> Site Information </a>
+                </div> 
+            </div>
       </div>
-    </body>
+</body>
