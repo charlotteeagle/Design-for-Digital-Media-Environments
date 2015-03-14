30aa6eff959c7c9d51220b70d8791f7745aa7361
chrislangley666 <chrislangley666@hotmail.co.uk>
2015-03-14 09:29:03 +0000

    Linking fix

--- includes/templates/menuheader.php
+++ includes/templates/menuheader.php
@@ -13,9 +13,9 @@
         <div class="buttons">
             <a href="../../public/structure/index.php" class="on">Home</a>
             <a href="../../public/structure/about.php">About Us</a>
-            <a href="../../public/help.php">Help</a>
+            <a href="../../public/structure/help.php">Help</a>
             <a href="http://dakar.bournemouth.ac.uk/~ceagle/wiki/dokuwiki/doku.php?id=start">Wiki</a>
-            <a href="../../public/signup.php">Sign Up</a>
+            <a href="../../public/structure/signup.php">Sign Up</a>
             <a href="../../public/structure/login.php">Login</a>
         </div>
      </div> 
