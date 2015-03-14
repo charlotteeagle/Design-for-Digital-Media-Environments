b904f572f700a193aed68cdfefd49993e3d24795
chrislangley666 <chrislangley666@hotmail.co.uk>
2015-03-14 08:47:55 +0000

    Organised public pages into file ’structure’. Another attempt to link pages, as they are not.

--- includes/templates/menuheader.php
+++ includes/templates/menuheader.php
@@ -11,8 +11,8 @@
     <div class="menu-header">   
         
         <div class="buttons">
-            <a href="../../public/index.php" class="on">Home</a>
-            <a href="../../public/about.php">About Us</a>
+            <a href="../../public/structure/index.php" class="on">Home</a>
+            <a href="../../public/structure/about.php">About Us</a>
             <a href="../../public/help.php">Help</a>
             <a href="http://dakar.bournemouth.ac.uk/~ceagle/wiki/dokuwiki/doku.php?id=start">Wiki</a>
             <a href="../../public/signup.php">Sign Up</a>
