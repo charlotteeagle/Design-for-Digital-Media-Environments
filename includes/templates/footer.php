cbffa5c6f75beed3b95a8225aa5ec10a3acdf567
chrislangley666 <chrislangley666@hotmail.co.uk>
2015-03-14 11:31:44 +0000

    Info page link fixed.

--- includes/templates/footer.php
+++ includes/templates/footer.php
@@ -5,7 +5,7 @@
         <div class="icon-container">
             <a href="https://en-gb.facebook.com/login"><img src="../../public/images/facebooklogo.png"></a>
             <a href="https://twitter.com/?lang=en-gb"><img src="../../public/images/twitterlogo.png"></a>
-            <a href="../../public/info.php"><img src="../../public/images/Info-50.png"></a>    
+            <a href="../../public/structure/info.php"><img src="../../public/images/Info-50.png"></a>    
         </div>
     </div>
 </body>
