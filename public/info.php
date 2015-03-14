dfdba9c6cadb65106ef98fc39a556d4ecc98c1d2
chrislangley666 <chrislangley666@hotmail.co.uk>
2015-03-14 08:43:58 +0000

    Attempt to link pages.

---
+++ public/info.php
@@ -0,0 +1,18 @@
+<?php 
+    require_once("../includes/connect.php");
+    require_once("../includes/functions.php"); 
+    include_once("../includes/templates/header.php");
+    include_once("../includes/templates/menuheader.php");  /* THE NEW NAV*/
+
+?>
+
+<body>
+
+
+</body>
+
+<!-- Begin Footer -->
+
+<?php include_once("../includes/templates/footer.php"); ?>   
+
+<!-- End Footer -->
