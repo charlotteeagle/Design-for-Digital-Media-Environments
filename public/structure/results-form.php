b2f0fcdd9af98f9b74a870d63badc0355bb4d04b
David <thedavidwatts@gmail.com>
2015-03-15 16:27:45 +0000

    edit to results-form values

--- public/structure/results-form.php
+++ public/structure/results-form.php
@@ -5,18 +5,18 @@
                     <br><br>
                          Location: <select name="house_location">
                             <option value="">--Select--</option>
-                            <option value="1">Bournemouth Centre</option>
-                            <option value="2">Winton</option>
-                            <option value="3">Boscombe</option>
-                            <option value="4">Kinson</option>
-                            <option value="5">Throop</option>
-                            <option value="6">Wallisdown</option>
-                            <option value="7">Southnourne</option>
-                            <option value="8">Iford</option>
-                            <option value="9">Christchurch</option>
-                            <option value="10">Westbourne</option>
-                            <option value="11">Parkstone</option>
-                            <option value="12">Northbourne</option>
+                            <option>Bournemouth Centre</option>
+                            <option>Winton</option>
+                            <option>Boscombe</option>
+                            <option>Kinson</option>
+                            <option>Throop</option>
+                            <option>Wallisdown</option>
+                            <option>Southnourne</option>
+                            <option>Iford</option>
+                            <option>Christchurch</option>
+                            <option>Westbourne</option>
+                            <option>Parkstone</option>
+                            <option>Northbourne</option>
                         </select> 
                     <br><br>
                     Property Type: <select name="house_type">
