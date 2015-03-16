23837e1e7a3e7f743b25ab374dc5a715c106ce9a
chrislangley666 <chrislangley666@hotmail.co.uk>
2015-03-16 16:38:47 +0000

    Re-Structure - UPLOAD

---
+++ public/structure/about.php
@@ -0,0 +1,20 @@
+<?php 
+    session_start();
+    require_once("../include/connect.php");     // Connect to database   
+    include 'structure/header.php'; // Include header
+    include 'structure/menuheader.php'; //Include menuheader
+  ?>
+
+<div class="main">
+    <div class="background-position" >            
+    </div> 
+</div>
+
+
+<!-- Begin Footer -->
+
+<?php 
+    include_once("structure/footer.php"); //Include footer
+?>  
+
+<!-- End Footer -->
---
+++ public/structure/menuheader.php
@@ -0,0 +1,28 @@
+
+<!doctype html>
+<html>
+    <head>
+        <title>Bournemouth Homes</title>
+        <link href="css/styles.css" rel="stylesheet">
+    </head>
+    
+
+<?php 
+        include 'loginform.php';
+?>
+   
+<!--Menu Header -->
+<body>
+    <div class="menu-header">   
+        
+        <div class="buttons">
+            <a href="../index.php" class="on">Home</a>
+            <a href="../../public/structure/about.php">About Us</a>
+            <a href="../../public/structure/help.php">Help</a>
+            <a href="http://dakar.bournemouth.ac.uk/~ceagle/wiki/dokuwiki/doku.php?id=start">Wiki</a>
+            <a href="../../public/structure/signup.php">Sign Up</a>
+        </div>
+     </div> 
+</body>    
+
+    
