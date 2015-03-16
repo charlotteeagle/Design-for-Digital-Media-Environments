2915594fcc5c1ac4bf238a77e32d987ebd7c92e4
David <thedavidwatts@gmail.com>
2015-03-16 17:47:46 +0000

    include updates

--- public/structure/form.php
+++ public/structure/form.php
@@ -1,9 +1,8 @@
 <?php 
     session_start();
-    require_once("../../includes/connect.php"); 
-    require_once("../../includes/functions.php"); 
-    include_once("../../includes/templates/header.php");
-    include_once("../../includes/templates/menuheader.php");
+    require_once("../../include/connect.php"); 
+    include_once("header.php");
+    include_once("menuheader.php");
 
 ?>
 
