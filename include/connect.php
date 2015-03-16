265db37561c53ed1d940b59d36f0464730a55f6a
David <thedavidwatts@gmail.com>
2015-03-16 18:10:55 +0000

    conect db change

--- include/connect.php
+++ include/connect.php
@@ -58,7 +58,7 @@ $success = mysqli_real_connect(
     define("DB_SERVER", "localhost");
     define("DB_USER", "root");
     define("DB_PASS", "root");
-    define("DB_NAME", "property"); 
+    define("DB_NAME", "Database3"); 
     define("DB_PORT", "8889");
 
     $connect = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME, DB_PORT);
