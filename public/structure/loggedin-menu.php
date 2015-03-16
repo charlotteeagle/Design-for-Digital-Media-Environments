0a5d4c6a7bb27c2a2ce423467cf9278905a1f5c9
chrislangley666 <chrislangley666@hotmail.co.uk>
2015-03-16 22:11:15 +0000

    Adding correct links - Test

--- public/structure/loggedin-menu.php
+++ public/structure/loggedin-menu.php
@@ -3,16 +3,15 @@
 <html>
     <head>
         <title>Bournemouth Homes</title>
-        <link href="css/styles.css" rel="stylesheet">
     </head>
 <body>
     <div class="menu-header">   
         <div class="buttons">    
             <a href="../index.php">Home</a>
-            <a href="loggedin-menu.php">About Us</a>
+            <a href="about.php">About Us</a>
             <a href="help.php">Help</a>
             <a href="http://dakar.bournemouth.ac.uk/~ceagle/wiki/dokuwiki/doku.php?id=start">Wiki</a>
-            <a href="add-property.php">Add Property</a>
+            <a href="form.php">Add Property</a>
         </div>
      </div> 
 </body>    
