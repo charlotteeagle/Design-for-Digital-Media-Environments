5e962cfce4eda1441cdfe0d44d36a1d693896dd6
David <thedavidwatts@gmail.com>
2015-03-16 00:42:04 +0000

    Comment removed

--- public/structure/form.php
+++ public/structure/form.php
@@ -92,52 +92,7 @@
 
 ?>
 
-<?php /* see http://www.w3schools.com/php/php_form_url_email.asp for help on this */
-  /* if(isset($_POST["submit"])) {
-        
-        if(empty($house_title)) {
-            $titleerr = "Invalid property title";
-        
-        }
-        
-        
-         if(empty($house_price)) {
-            $priceerr = "Invalid Price";
-        
-        }
-        
-        
-        if(empty($house_location)) {
-            $locationerr = "Invalid Location";
-        
-        }
-        
-        if(empty($house_type)) {
-            $typeerr = "Invalid Property Type";
-        
-        }
-        
-        if(empty($house_bedroomnumber)) {
-            $numbererr = "Invalid Bedroom Number";
-        
-        }
-        
-        if(empty($house_photo)) {
-            $photoerr = "Invalid Photo";
-        
-        }
-        
-        if(empty($house_description)) {
-            $descriptionerr = "Invalid Description";
-        }
-        
-    }
-      
-        
-    
-*/
 
-?>
 
 <!doctype html>
 <html>
@@ -154,8 +109,6 @@
                 ?>
             
                
-        
-        
             
                 <h2>Create Your Listing</h2>
                 <p><span class="error">* required field.</span></p> 
