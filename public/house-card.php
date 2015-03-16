e072362845484f680366689fd097ff77857e4735
chrislangley666 <chrislangley666@hotmail.co.uk>
2015-03-16 17:04:37 +0000

    Attempt to re-upload.

--- public/house-card.php
+++
@@ -1,9 +0,0 @@
-<p>HOUSE CARD</p>
-
-<?php
-    while($row = mysqli_fetch_assoc($result)) {
-        echo $house_title;
-    }
-?>
-
-<p><?php echo $house_title; ?></p>
--- public/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-    session_start();
-    session_destroy(); //Logged out
-    header("Location: index.php");
-?>
