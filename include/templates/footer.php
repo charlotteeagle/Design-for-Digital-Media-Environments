9b79110279e99e220a774a3fefb8dc92e7ca2792
David <thedavidwatts@gmail.com>
2015-03-17 18:48:42 +0000

    Footer link change

--- include/templates/footer.php
+++ include/templates/footer.php
@@ -5,7 +5,7 @@
         <div class="icon-container">
             <a href="https://en-gb.facebook.com/login"><img src="images/facebooklogo.png"></a>
             <a href="https://twitter.com/?lang=en-gb"><img src="images/twitterlogo.png"></a>
-            <a href="../../public/structure/info.php"><img src="images/Info-50.png"></a>    
+            <a href="info.php"><img src="images/Info-50.png"></a>    
         </div>
     </div>
 </body>
