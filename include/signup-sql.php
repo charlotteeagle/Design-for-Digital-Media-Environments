c91fc945f164d04ec2f04b4f4f05e7d3bf884365
chrislangley666 <chrislangley666@hotmail.co.uk>
2015-03-19 17:42:09 +0000

    Directory fix

--- include/signup-sql.php
+++ include/signup-sql.php
@@ -21,29 +21,29 @@
     if(isset($_POST["signup"])) {                    
         if(empty($username)) {                             
             $message = "You need to create a username!";
-                header("Location: signup.php");
+                header("Location: ../public/signup.php");
         } else if(empty($password)) {
             $message = "You need to create a password!";
-                header("Location: signup.php");
+                header("Location: ../public/signup.php");
         } else if(empty($firstname)) {
             $message = "You didn't input your firstname!";
-                header("Location: signup.php");
+                header("Location: ../public/signup.php");
         } else if(empty($lastname)) {
             $message = "You didn't input your lastname!";
-                header("Location: signup.php");
+                header("Location: ../public/signup.php");
         }else if(empty($email)) {
             $message = "Remember to add your email address!";
-                header("Location: signup.php");
+                header("Location: ../public/signup.php");
         } else {                                        
             $query = "INSERT INTO users (username, password, firstname, lastname, email) VALUES ('{$username}', '{$password}', '{$firstname}', '{$lastname}', '{$email}')";
             $result = mysqli_query($connect, $query);   
         }
             if($result) {                             
                 $message = "Success! You've been registered to Bournemouth Homes";   
-                header("Location: signup.php");
+                header("Location: ../public/signup.php");
             } else {                                    
                 $message = "Error, something went wrong!";
-                header("Location: signup.php");
+                header("Location: ../public/signup.php");
             }
     }
 ?>
