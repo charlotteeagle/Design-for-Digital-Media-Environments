87cfefdb9547adead68446ab3b8823a58cf71c11
David <thedavidwatts@gmail.com>
2015-03-19 22:43:48 +0000

    Added user id to session - login.php

--- include/login.php
+++ include/login.php
@@ -65,6 +65,7 @@
         $query = "SELECT * FROM users WHERE username='{$username}' AND password='{$password}' LIMIT 1"; // Get username and password
         $result = mysqli_query($connect, $query); 
         if ($user = mysqli_fetch_assoc($result)) {      // started a session so user can login
+            $_SESSION["id"] = $user["id"];
             $_SESSION["user"] = $user["username"];      // Equal to login details on the database
             $_SESSION["name"] = $user["firstname"];
             $_SESSION["message"] = "";
