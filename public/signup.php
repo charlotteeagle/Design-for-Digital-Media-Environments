8e71d8b4e81f678d822d136d3f2a0e9b37fe1a5c
chrislangley666 <chrislangley666@hotmail.co.uk>
2015-03-19 16:07:29 +0000

    Values weren’t correct, hopefully are now.

--- public/signup.php
+++ public/signup.php
@@ -10,8 +10,8 @@
          <div class="login-box">
             <form action="index.php" method="post">
             <p>First Name:</p><input type="text" name="firstname" value=""/>
-            <p>Surname:</p><input type="text" name="surname" value=""/>
-            <p>Email Address:</p><input type="text" name="email_address" value=""/>
+            <p>Surname:</p><input type="text" name="lastname" value=""/>
+            <p>Email Address:</p><input type="text" name="email" value=""/>
             <p>Username:</p> <input type="text" name="username" value=""/>
             <p>Password:</p><input type="text" name="password" value=""/>
             
