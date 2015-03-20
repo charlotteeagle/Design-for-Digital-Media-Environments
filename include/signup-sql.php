67b23fb5c957a57b9b7be7bb3c2d2d0f14084b64
chrislangley666 <chrislangley666@hotmail.co.uk>
2015-03-20 11:45:44 +0000

    Attempted to create error message when user doesn’t input data.

--- include/signup-sql.php
+++ include/signup-sql.php
@@ -18,7 +18,18 @@
     }
 ?>
 <?php                                                   
-    if(isset($_POST["signup"])) {                                                           
+    if(isset($_POST["signup"])) {    
+        if(empty($username)) {                              // If any empty fields, display an error message
+            $message = "Please create a username.";
+        } else if(empty($password)) {
+            $message = "Please create a password.";
+        } else if(empty($firstname)) {
+            $message = "Please state your firstname.";
+        } else if(empty($lastname)) {
+            $message = "Please state your lastname";
+        }else if(empty($email)) {
+            $message = "Please enter your email";
+    
             $query = "INSERT INTO users (username, password, firstname, lastname, email) VALUES ('{$username}', '{$password}', '{$firstname}', '{$lastname}', '{$email}')";
             $result = mysqli_query($connect, $query);   
         
@@ -29,5 +40,6 @@
                 $_SESSION['message'] = "Error! You missed something!";
                 header("Location: ../public/signup.php");
             }
+        }
     }
 ?>
