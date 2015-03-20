9665e4906c91de5154ebbf4f72bba5440ea0633a
chrislangley666 <chrislangley666@hotmail.co.uk>
2015-03-20 17:45:07 +0000

    Added sort option.

---
+++ include/sort-box.php
@@ -0,0 +1,9 @@
+<div class="sort-box">
+        <form action="index.php" method="post">
+            <select name="sort-by">
+                <option value="new">New</option>
+                <option value="old">Old</option>
+            </select>
+            <input type="submit" name="sort" value="Sort" />
+         </form>
+</div>
--- public/index.php
+++ public/index.php
@@ -46,6 +46,20 @@
     
 </div>
 
+ <?php if(isset($_SESSION["user"])) { ?>
+            <div class="box">
+                <form action="index.php" method="post">
+                    <select name="sort-by">
+                        <option value="new">New</option>
+                        <option value="old">Old</option>
+                    </select>
+                    <input type="submit" name="sort" value="Sort" />
+                </form>
+
+            </div>
+            
+            <?php } ?>
+
 
     
     <div class="house-card">
