38ff98d5718560cce928a10f2cfd6f4a05ceed96
chrislangley666 <chrislangley666@hotmail.co.uk>
2015-03-20 13:40:02 +0000

    TEST

--- include/login.php
+++ include/login.php
@@ -1,82 +1,29 @@
-
-<!-------------------------------------------------------- Add Property ---------------------------------------------------------------->
-
-
-<?php     /*                                              // Add 
-    if(isset($_POST["submit"])) {  
-        $house_title = ucfirst($_POST["house_title"]);                // Insert each field with upper case for first letter
-        $house_price = ucfirst($_POST["house_price"]);  
-        $house_location = ucfirst($_POST["house_location"]);      
-        $house_type = ucfirst($_POST["house_type"]);    
-        $house_bedroomnumber = ucfirst($_POST["house_bedroomnumber"]);                
-        $house_photo = ucfirst($_POST["house_photo"]);              
-        $house_description = ucfirst($_POST["house_description"]);            
-    } else { 
-        $house_title = "";                                     // Leave each field blank
-        $house_price = "";                              
-        $house_location = "";                                
-        $house_type ="";                                
-        $house_bedroomnumber = "";                                     
-        $house_photo = "";                                    
-        $house_description ="";                                    
-    }
-?>
-
-<?php                                                   // Add user's property to database and display on website
-    if(isset($_POST["submit"])) {                       // If post has been submitted
-        if(empty($house_title)) {                              // If any empty fields, display an error message
-            $message = "You've forgotten to add a house title!";
-        } else if(empty($house_price)) {
-            $message = "You've forgotten to add a house price!";
-        } else if(empty($house_location)) {
-            $message = "You've forgotten to add a house location!";
-        } else if(empty($house_type)) {
-            $message = "You've forgotten to add a house type!";
-        }else if(empty($house_bedroomnumber)) {
-            $message = "You've forgotten to add a house bedroom number!";
-        }else if(empty($house_photo)) {
-            $message = "You've forgotten to add a house photo!";
-        }else if(empty($house_description)) {
-            $message = "Remember to select add a house description!";
-        } else {                                        // Otherwise 
-            $query = "INSERT INTO posts (house_title, house_price, house_location, house_type, house_bedroomnumber, house_photo, house_description) VALUES ('{$house_title}', '{$house_price}', '{$house_location}', '{$house_type}', '{$house_bedroomnumber}', '{$house_photo}', '{$house_description}')";
-            $result = mysqli_query($connect, $query);   
-            if($result) {                             
-                $message = "Success! Your property has been listed!";   
-            } else {                                    // Otherwise display unsuccessful message
-                $message = "Oh dear, your listing was unsuccessful."; 
-            }
-            $house_title = "";                                 // Get user to fill in fields required 
-            $house_price = ""; 
-            $house_location = ""; 
-            $house_type ="";
-            $house_bedroomnumber = "";
-            $house_photo = "";
-            $house_description = "";
-        }
-    }   */
-?>
 <!-------------------------------------------------------- Login ---------------------------------------------------------------->
 <?php
 /*User login*/
-    if(isset($_POST["login"])) {                        // If login is successful
+    if(isset($_POST["login"])) {   
+        
+        $username = $_POST["username"];
+        $password = $_POST["password"];
+        
         if(empty($username)) {                              // If any empty fields, display an error message
             $_SESSION['message'] =  "Please enter your username.";
         } else if(empty($password)) {
              $_SESSION['message'] =  "Please enter your password.";
         } else {
+            
         $query = "SELECT * FROM users WHERE username='{$username}' AND password='{$password}' LIMIT 1"; // Get username and password
         $result = mysqli_query($connect, $query); 
+        
         if ($user = mysqli_fetch_assoc($result)) {      // started a session so user can login
             $_SESSION["id"] = $user["id"];
             $_SESSION["user"] = $user["username"];      // Equal to login details on the database
             $_SESSION["name"] = $user["firstname"];
+            $_SESSION["message"] = "Successfully logged in. Welcome back {$user["username"]}.";
+        } else {
+            $_SESSION["message"] = "Wrong username/password.";
         }
-        if ($result) {
-            $_SESSION["message"] = "";
-        } else {                                        // Otherwise
-            $_SESSION["message"] = "Unsuccessful";     // Display message with error logging in
-        }
+        
      }
   }
   
--- include/loginform.php
+++ include/loginform.php
@@ -4,7 +4,9 @@
     <p><?php echo $_SESSION["user"]; ?></p>
         <a href="logout.php"> Logout</a>
 
-<?php } else { ?> <!-- Otherwise, display form to login-->
+<?php } 
+
+    else { ?> <!-- Otherwise, display form to login-->
     <form action="index.php" method="post">
             <p>Username:</p><input type="text" name="username" value=""/>
              <p>Password:</p><input type="text" name="password" value=""/>
