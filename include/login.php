46b1492bb88d100604470edc6d0c7e4312a526b9
chrislangley666 <chrislangley666@hotmail.co.uk>
2015-03-20 12:43:39 +0000

    Login authentication test/

--- include/login.php
+++ include/login.php
@@ -65,13 +65,14 @@
         } else if(empty($password)) {
              $_SESSION['message'] =  "Please enter your password.";
         } else {
-        
         $query = "SELECT * FROM users WHERE username='{$username}' AND password='{$password}' LIMIT 1"; // Get username and password
         $result = mysqli_query($connect, $query); 
         if ($user = mysqli_fetch_assoc($result)) {      // started a session so user can login
             $_SESSION["id"] = $user["id"];
             $_SESSION["user"] = $user["username"];      // Equal to login details on the database
             $_SESSION["name"] = $user["firstname"];
+        }
+        if ($result) {
             $_SESSION["message"] = "";
         } else {                                        // Otherwise
             $_SESSION["message"] = "Unsuccessful";     // Display message with error logging in
