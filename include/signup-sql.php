3f59812a3a8907580e8fe62e018a2ebfed5d661e
chrislangley666 <chrislangley666@hotmail.co.uk>
2015-03-20 11:59:34 +0000

    Taken out re-direction to see if the div still displays without having to re-direct. TEST

--- include/signup-sql.php
+++ include/signup-sql.php
@@ -21,19 +21,19 @@
     if(isset($_POST["signup"])) {    
         if(empty($username)) {                             // If any empty fields, display an error message
             $_SESSION['message'] =  "Please create a username.";
-            header("Location: ../public/signup.php");
+            
         } else if(empty($password)) {
             $_SESSION['message'] = "Please create a password.";
-            header("Location: ../public/signup.php");
+            
         } else if(empty($firstname)) {
             $_SESSION['message'] =  "Please state your firstname.";
-            header("Location: ../public/signup.php");
+            
         } else if(empty($lastname)) {
             $_SESSION['message'] =  "Please state your lastname";
-            header("Location: ../public/signup.php");
+       
         } else if(empty($email)) {
             $_SESSION['message'] =  "Please enter your email";
-            header("Location: ../public/signup.php");
+      
 
             $query = "INSERT INTO users (username, password, firstname, lastname, email) VALUES ('{$username}', '{$password}', '{$firstname}', '{$lastname}', '{$email}')";
             $result = mysqli_query($connect, $query);   
