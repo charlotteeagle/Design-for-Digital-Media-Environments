a67eaa7a36c1250e4b1d147876b8c427a0ac2ab9
chrislangley666 <chrislangley666@hotmail.co.uk>
2015-03-20 12:47:29 +0000

    Authentication test. When user clicks login, the form is sent to form.php and runs the if statements.

--- include/loginform.php
+++ include/loginform.php
@@ -5,7 +5,7 @@
         <a href="logout.php"> Logout</a>
 
 <?php } else { ?> <!-- Otherwise, display form to login-->
-    <form action="index.php" method="post">
+    <form action="login.php" method="post">
             <p>Username:</p><input type="text" name="username" value=""/>
              <p>Password:</p><input type="text" name="password" value=""/>
             <input type="submit" id="submit" name="login" value="Login">
