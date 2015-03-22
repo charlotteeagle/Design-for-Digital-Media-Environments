52f22006d33c0ce7e9eb1f6611d4160a6d75766f
chrislangley666 <chrislangley666@hotmail.co.uk>
2015-03-22 20:43:26 +0000

    background sift attempt

--- public/index.php
+++ public/index.php
@@ -5,7 +5,15 @@
     include '../include/templates/header.php'; // Include header
     include '../include/templates/menuheader.php';
 ?>
-      
+ 
+<?php
+  $bg = array('b1.jpg', 'b2.jpg', 'b3.jpg', 'b4.jpg', 'b5.jpg', 'b6.jpg', 'b7.jpg' ); // array of filenames
+
+  $i = rand(0, count($bg)-1); // generate random number size of the array
+  $selectedBg = "$bg[$i]"; // set variable equal to which random filename was chosen
+?>
+
+
 <div class="main">
     <?php  if(isset($_SESSION["user"])) { ?>
     
