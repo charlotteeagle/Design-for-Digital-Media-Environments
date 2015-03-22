855299b8bfd0355917a73364a465124a47fcedfd
chrislangley666 <chrislangley666@hotmail.co.uk>
2015-03-22 14:56:36 +0000

    Added jquery. Attempt login box animation.

---
+++ public/js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    
+    //console.log("jquery works"); //used to test results.
+    
+    $(".login-box").hide(); //selecting element, ie login box. And hiding it. 
+    
+    $(".toggle-box").click(function() { //click function with response 
+        $(".login-box").slideToggle();
+                           
+    });
+    
+    
+    
+    
+});
