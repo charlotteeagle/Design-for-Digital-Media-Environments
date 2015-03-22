876afd32b0db9ddf9371958e08815497df4c20dc
chrislangley666 <chrislangley666@hotmail.co.uk>
2015-03-22 17:44:01 +0000

    Font change

--- include/templates/header.php
+++ include/templates/header.php
@@ -3,7 +3,7 @@
     <head>
         <title>Bournemouth Homes</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
-        <link href='http://fonts.googleapis.com/css?family=Play' rel='stylesheet' type='text/css'> 
+        <link href='http://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'> 
         <link href='css/normal.css' rel='stylesheet' type='text/css'> 
         <link href='css/styles.css' rel='stylesheet' type='text/css'> 
     </head>
