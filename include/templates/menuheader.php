6459480ff29ba671f4de82fbe948c465cd8e443e
chrislangley666 <chrislangley666@hotmail.co.uk>
2015-03-22 18:37:33 +0000

    TEST

--- include/templates/menuheader.php
+++ include/templates/menuheader.php
@@ -12,10 +12,9 @@
             <a href="help.php">Help</a>
             <a href="http://dakar.bournemouth.ac.uk/~ceagle/wiki/dokuwiki/doku.php?id=start">Wiki</a>
             <a href="signup.php">Sign Up</a>
-        </div>
-    </div> 
-
 <?php 
     include '../include/loginform.php';
 ?>
+        </div>
+    </div> 
 </body>    
