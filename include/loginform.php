b0169f2fb2dbd099d569d4f58561173e2f8b1cc4
chrislangley666 <chrislangley666@hotmail.co.uk>
2015-03-22 15:16:51 +0000

    jquery test.

--- include/loginform.php
+++ include/loginform.php
@@ -1,17 +1,17 @@
-<div class="login-box">
-    <p>Login</p>
+<div class="toggle-box">
+
 <?php if(isset($_SESSION["user"])) { ?> <!-- If user is logged in, display logout button -->
         <a href="logout.php"> Logout</a>
 
 <?php } 
 
     else { ?> <!-- Otherwise, display form to login-->
-    <div class="toggle-box">
-        <form action="index.php" method="post">
+    <div class="login-box"></div>
+    <form action="index.php" method="post">
             <p>Username:</p><input type="text" name="username" value=""/>
              <p>Password:</p><input type="text" name="password" value=""/>
             <input type="submit" id="submit" name="login" value="Login">
-        </form>
-    </div>
+    </form>
+
 <?php } ?> 
 </div>
