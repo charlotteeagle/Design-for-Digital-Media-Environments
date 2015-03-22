08317ecd6d1b98cee386f6e85899629454888cdc
chrislangley666 <chrislangley666@hotmail.co.uk>
2015-03-22 13:47:35 +0000

    Took our boolean.

--- include/house-card.php
+++ include/house-card.php
@@ -23,7 +23,7 @@
         
      $result = mysqli_query($connect, $query); 
         
-     while($row = mysqli_fetch_assoc($result)) {
+     ($row = mysqli_fetch_assoc($result)) {
         echo $row["house_id"];
         echo $row["house_title"];
         echo $row["house_location"];
