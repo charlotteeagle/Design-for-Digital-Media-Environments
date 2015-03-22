038965a0d343a85d1b4aee00957c33b64a92741d
chrislangley666 <chrislangley666@hotmail.co.uk>
2015-03-22 16:29:56 +0000

    Menu header change.

--- include/templates/footer.php
+++ include/templates/footer.php
@@ -2,8 +2,7 @@
     <div class="footer">
         <div class="icon-container">
             <a href="https://en-gb.facebook.com/login"><img src="images/facebooklogo.png"></a>
-            <a href="https://twitter.com/?lang=en-gb"><img src="images/twitterlogo.png"></a>
-            <a href="info.php"><img src="images/Info-50.png"></a>    
+            <a href="https://twitter.com/?lang=en-gb"><img src="images/twitterlogo.png"></a>   
         </div>
     </div>
   <script></script>
