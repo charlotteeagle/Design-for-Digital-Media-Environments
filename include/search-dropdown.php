1050bb353692e9bdf8901dcaebfea4220377d1fb
chrislangley666 <chrislangley666@hotmail.co.uk>
2015-03-22 17:46:05 +0000

    AAAAA Test

--- include/search-dropdown.php
+++ include/search-dropdown.php
@@ -1,18 +1,17 @@
 <form action="index.php" method="post"> 
  Search:<select name="house_id">
-        <option value="1">Bournemouth Centre</option>
-        <option value="2">Winton</option>
-        <option value="3">Boscombe</option>
-        <option value="4">Kinson</option>
-        <option value="5">Throop</option>
-        <option value="6">Wallisdown</option>
-        <option value="7">Southnourne</option>
-        <option value="8">Iford</option>
-        <option value="9">Christchurch</option>
-        <option value="10">Westbourne</option>
-        <option value="11">Parkstone</option>
-        <option value="12">Northbourne</option>
-    
-        <input type="submit" name="search" value="Search!"/>
+            <option value="1">Bournemouth Centre</option>
+            <option value="2">Winton</option>
+            <option value="3">Boscombe</option>
+            <option value="4">Kinson</option>
+            <option value="5">Throop</option>
+            <option value="6">Wallisdown</option>
+            <option value="7">Southnourne</option>
+            <option value="8">Iford</option>
+            <option value="9">Christchurch</option>
+            <option value="10">Westbourne</option>
+            <option value="11">Parkstone</option>
+            <option value="12">Northbourne</option>
+            <input type="submit" name="search" value="Search!"/>
         </select> 
 </form>
