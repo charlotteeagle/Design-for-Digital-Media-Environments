a3af076afcaad8572e4e443c060543f6be40064d
chrislangley666 <chrislangley666@hotmail.co.uk>
2015-03-23 12:45:20 +0000

    Result test

--- include/house-card-sql.php
+++ include/house-card-sql.php
@@ -3,20 +3,21 @@
         $query = "SELECT * FROM posts";
         $result = mysqli_query($connect, $query); 
         print_r($result);
+ } ?>
         
-     while($row = mysqli_fetch_assoc($result)) {
-        
-        echo $row["user_id"];
-        echo $row["house_title"];
-        echo $row["house_price"];
-        echo $row["house_location"];
-        echo $row["house_type"];
-        echo $row["house_bedroomnumber"];
-        echo $row["house_photo"];
-        echo $row["house_description"]; 
-        echo $row["date_added"]; 
-     }
- }
+<?php//  while($row = mysqli_fetch_assoc($result)) {
+       // 
+        //echo $row["user_id"];
+        //echo $row["house_title"];
+        //echo $row["house_price"];
+        //echo $row["house_location"];
+        //echo $row["house_type"];
+        //echo $row["house_bedroomnumber"];
+        //echo $row["house_photo"];
+        //echo $row["house_description"]; 
+        //echo $row["date_added"]; 
+    // }
+ 
 ?>
 
 <?php 
