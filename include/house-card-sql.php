0effe30216957934c456858688946600c00aadb0
chrislangley666 <chrislangley666@hotmail.co.uk>
2015-03-23 16:07:11 +0000

    Sort button issue fix

--- include/house-card-sql.php
+++ include/house-card-sql.php
@@ -1,26 +1,28 @@
 <?php
  if(isset($_POST["location_id"])) {
         $location = $_POST["location_id"];
-        $query = "SELECT * FROM posts WHERE house_location ='$location'";
+        $_SESSION = $location;
+        $query = "SELECT * FROM posts WHERE house_location ='$location' ORDER BY id DESC";
         $result = mysqli_query($connect, $query); 
- } ?>
-
-<?php 
-
-    if(isset($_POST["sort-by"])) {
-        $sort = $_POST["sort-by"];
-        
-        if(strcmp($sort, "new") == 0) {
-            $query = "SELECT * FROM posts ORDER BY id DESC";
-            $result = mysqli_query($connect, $query); 
+        // If form request comes from Search Button
+ } elseif(isset($_SESSION["location"])) {
+        // Form request comes from Sort By Button
+        $location = $_SESSION["location"];
+        // Use $_SESSION["location"] as a way of remembering our last location choice
+        if(isset($_POST["sort-by"])) {
+            $sort = $_POST["sort-by"];
+            if(strcmp($sort, "new") == 0) {
+                $query = "SELECT * FROM posts WHERE house_location ='$location' ORDER BY id DESC";
+                $result = mysqli_query($connect, $query); 
+            } else {
+                $query = "SELECT * FROM posts WHERE house_location ='$location' ORDER BY id ASC";
+                $result = mysqli_query($connect, $query); 
+            }
         } else {
-            $query = "SELECT * FROM posts ORDER BY id ASC";
+            $query = "SELECT * FROM posts WHERE house_location ='$location' ORDER BY id DESC";
             $result = mysqli_query($connect, $query); 
         }
-        
-    } else {
-        $query = "SELECT * FROM posts ORDER BY id DESC";
-        $result = mysqli_query($connect, $query); 
-    }
+ } 
+     
 
-?>
+?> 
