68c60fd9dc199d5dbf917cc98aa4e7ace0cfd6c6
chrislangley666 <chrislangley666@hotmail.co.uk>
2015-03-23 13:24:55 +0000

    RESULT TEST

---
+++ include/house-card.php
@@ -0,0 +1,39 @@
+<div class="house-card">
+                
+                <div class="house-image">
+                <?php echo $row["house_photo"]; ?>
+                </div>
+                
+                <div class="house-title">
+                <?php echo $row["user_id"]; ?>
+                </div>
+                    
+                <div class="house-title">
+                <?php echo $row["house_title"]; ?>
+                </div>
+                
+                <div class="house-price">
+                <?php echo $row["house_price"]; ?>
+                </div>
+                
+                <div class="house-location">
+                <?php echo $row["house_location"]; ?>
+                </div>
+                
+                <div class="house-type">
+                <?php echo $row["house_type"]; ?>
+                </div>
+                
+                <div class="house-bedroomnumber">
+                <?php echo $row["house_bedroomnumber"]; ?>
+                </div>
+                
+                <div class="house-description">
+                <?php echo $row["house_description"]; ?>
+                </div>
+                
+                
+                <div class="date-added">
+                <?php echo $row["date_added"]; ?>
+                </div>
+            </div>
