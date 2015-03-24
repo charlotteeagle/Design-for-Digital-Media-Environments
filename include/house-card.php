a14f7045a529dc6fdc9cd2c94b4fac5caf8f0bc2
chrislangley666 <chrislangley666@hotmail.co.uk>
2015-03-24 16:03:52 +0000

    Display test

--- include/house-card.php
+++ include/house-card.php
@@ -4,14 +4,14 @@
                 <p style="display:inline;">User ID: <?php echo $row["user_id"]; ?></p><p style="display:inline;"> Date added:<?php echo $row["date_added"]; ?></p>
                 </div>
                 
+                <div class="house-price">
+                <p>£<?php echo $row ["house_price"]; ?></p>
+                </div>
+                
                 <div class="house-image">
                 <?php echo $row["house_photo"]; ?>
                 </div>
                     
-                <div class="house-price">
-                <p>£<?php echo $row ["house_price"]; ?></p>
-                </div>
-    
                 <div class="house-title">
                 <?php echo $row["house_title"]; ?>
                 </div>
