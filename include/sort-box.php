48ea2b6ec0b7917a4c077a95a19afe7bed129d4f
chrislangley666 <chrislangley666@hotmail.co.uk>
2015-03-24 13:36:37 +0000

    Removed separate sort box

--- include/sort-box.php
+++
@@ -1,9 +0,0 @@
-<div class="sort-box">
-        <form action="../public/index.php" method="post">
-            <select name="sort-by">
-                <option value="new">New</option>
-                <option value="old">Old</option>
-            </select>
-            <input type="submit" name="sort" value="Sort" />
-         </form>
-</div>
