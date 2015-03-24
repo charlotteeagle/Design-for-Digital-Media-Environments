460ff6723609161787e17bffd6f4b8ca0474826f
chrislangley666 <chrislangley666@hotmail.co.uk>
2015-03-24 13:20:33 +0000

    Sort button TEST

--- include/house-card-sql.php
+++ include/house-card-sql.php
@@ -1,27 +1,21 @@
 <?php
  if(isset($_POST["location_id"])) {
-        $location = $_POST["location_id"];
-        $_SESSION = $location;
-        $query = "SELECT * FROM posts WHERE house_location ='$location' ORDER BY id DESC";
+        $location = $_POST["location_id"]
+        $sortOrder = $_POST["sort-by"];
+     
+        if(strcmp($sort, "new") == 0) {
+            $order = "DESC";
+        } else {
+            $order = "ASC";
+        }
+     
+        $query = "SELECT * FROM posts WHERE house_location ='$location' ORDER BY id '{$order}'";
         $result = mysqli_query($connect, $query); 
         // If form request comes from Search Button
  } elseif(isset($_SESSION["location"])) {
         // Form request comes from Sort By Button
         $location = $_SESSION["location"];
         // Use $_SESSION["location"] as a way of remembering our last location choice
-        if(isset($_POST["sort-by"])) {
-            $sort = $_POST["sort-by"];
-            if(strcmp($sort, "new") == 0) {
-                $query = "SELECT * FROM posts WHERE house_location ='$location' ORDER BY id DESC";
-                $result = mysqli_query($connect, $query); 
-            } else {
-                $query = "SELECT * FROM posts WHERE house_location ='$location' ORDER BY id ASC";
-                $result = mysqli_query($connect, $query); 
-            }
-        } else {
-            $query = "SELECT * FROM posts WHERE house_location ='$location' ORDER BY id DESC";
-            $result = mysqli_query($connect, $query); 
-        }
  } 
      
 
