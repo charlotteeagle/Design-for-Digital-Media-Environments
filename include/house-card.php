07030c54d21d9d951cadb164e1e5ffdfc6f42f8a
chrislangley666 <chrislangley666@hotmail.co.uk>
2015-03-25 13:34:09 +0000

    TEST

--- include/house-card.php
+++ include/house-card.php
@@ -19,20 +19,19 @@
                 
                     <div class="house-title">
                         <?php echo $row["house_title"]; ?>
-                    
-
-                        <div class="house-type">
-                            <p>House Type: <?php echo $row["house_type"]; ?></p>
-                        </div>
-
-                        <div class="house-bedroomnumber">
-                            <p>Bedroom Number: <?php echo $row["house_bedroomnumber"]; ?></p>
-                        </div>
-
-                        <div class="house-description">
-                            <?php echo $row["house_description"]; ?>
-                        </div> 
                     </div>
+
+                    <div class="house-type">
+                        <p>House Type: <?php echo $row["house_type"]; ?></p>
+                    </div>
+
+                    <div class="house-bedroomnumber">
+                        <p>Bedroom Number: <?php echo $row["house_bedroomnumber"]; ?></p>
+                    </div>
+
+                    <div class="house-description">
+                        <?php echo $row["house_description"]; ?>
+                    </div>  
                     
                 </div>
                          
