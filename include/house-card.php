8d3df7936614d5335a8fcd7d4bdfc2e975f45bc7
chrislangley666 <chrislangley666@hotmail.co.uk>
2015-03-26 15:07:47 +0000

    TETS

--- include/house-card.php
+++ include/house-card.php
@@ -1,12 +1,11 @@
 <div class="house-card">
-                
-                  <div class="house-card-position">
-                
+
                 <div class="user-id-box">
                     <p style="display:inline;">User ID: <?php echo $row["user_id"]; ?></p>
                     <p style="display:inline;"> Date added:<?php echo $row["date_added"]; ?></p>
                     
                 </div>
+    
                 <div class="house-price">
                         <?php echo $row ["house_price"]; ?>
                 </div>
@@ -14,41 +13,22 @@
                 <div class="house-location">
                         <?php echo $row["house_location"]; ?>
                 </div>
-                    
-                </div>
-                
-                <div class="house-card-position">
-              
-    
-                    <div class="house-image">
+            
+                <div class="house-image">
                         <?php echo $row["house_photo"]; ?>
                     
-                    </div>
-                
-                    
-                <div class="clearfix">
-                    
-                    <div class="house-title">
+                </div>
+
+                <div class="house-title">
                         <?php echo $row["house_title"]; ?>
-                    </div>
+                </div>
+
+                <div class="house-type">
+                        <p>House Type: <?php echo $row["house_type"]; ?></p>
                 </div>
                 
-                
-            
-                    <div class="house-type">
-                        <p>House Type: <?php echo $row["house_type"]; ?></p>
-                    </div>
-                
-    
-
-                    <div class="house-bedroomnumber">
+                <div class="house-bedroomnumber">
                         <p>Bedroom Number: <?php echo $row["house_bedroomnumber"]; ?></p>
-                    </div>
-                    </div>
-                    
-                </div>    
-                
-                
-                    
-                         
+                </div>
+                                    
 </div>
