4157ac20aee61368a320cd3567bb451792385c24
chrislangley666 <chrislangley666@hotmail.co.uk>
2015-03-26 18:33:43 +0000

    test

--- include/house-card.php
+++ include/house-card.php
@@ -31,7 +31,7 @@
                 </div>
     
                 <div class="more-info">
-                  <a href link="house-card-info.php" id="<?php echo $row ["id"];?>" value="More Info" type="submit"> </a>
+                  <a href link="house-card-info.php" id="<?php echo $row ["id"];?>" > </a>
                 </div>
                 
 </div>
