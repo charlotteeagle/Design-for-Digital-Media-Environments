43c706df3938a21ef49011992434e2a2d5eb3be8
chrislangley666 <chrislangley666@hotmail.co.uk>
2015-03-26 20:04:51 +0000

    TEST

--- public/house-card-info.php
+++ public/house-card-info.php
@@ -31,53 +31,49 @@ $house_description = $value->house_description;
 $house_photo_path = $value->house_photo_path; 
 ?>  
 
- <div class="house-card">
-                    
+
+<div class="house-card">
+    
+    
+                <div class="house-image">
+                  <a href="<?php echo $house_photo_path; ?>">
+                        
+                    <img style="width: 290px; height: 230;" alt="OOPS the Image is Missing" src="<?php echo $house_photo_path; ?>"> 
+                
                 <div class="user-id-box">
                     <p style="display:inline;">User ID: <?php echo $user_id; ?></p>
                     <p style="display:inline;"> Date added:<?php echo $date_added; ?></p>
-                    
                 </div>
-                    <div class="house-price">
+    
+                <div class="house-price">
                         £ &nbsp;<?php echo $house_price; ?>
-                    </div>
+                </div>
 
-                    <div class="house-location">
+                <div class="house-location">
                         <?php echo $house_location; ?>
-                        
-                    </div>
-                
-                
-                <div class="house-image">
-            
-                    
-                    <a href="<?php echo $house_photo_path; ?>">
-                        
-                    <img style="width: 290px; height: 230;" alt="OOPS the Image is Missing" src="<?php echo $house_photo_path; ?>">
-                        
-                    </a>
-                    
-                    <div class="house-title">
+                </div>
+        
+                <div class="house-title">
                         <?php echo $house_title; ?>
-                    </div>
+                </div>
 
-                    <div class="house-type">
+                <div class="house-type">
                         <p>House Type: <?php echo $house_type; ?></p>
-                    </div>
-
-                    <div class="house-bedroomnumber">
-                        <p>Number of Bedrooms: <?php echo $house_bedroomnumber; ?></p>
-                    </div>
-
-                    <div class="house-description">
+                </div>
+                
+                <div class="house-bedroomnumber">
+                       <p>Number of Bedrooms: <?php echo $house_bedroomnumber; ?></p>
+                </div>
+                 
+                </div>
+    
+               <div class="house-description">
                         <?php echo $house_description; ?>
                   
-                    </div>
                 </div>
-                         
+           
+              
 </div>
-
-
 
  
     
