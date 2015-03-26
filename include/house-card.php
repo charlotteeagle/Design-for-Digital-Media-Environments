a2f6bd32a28a9b6a330fe4cfa4ef65ddb580b9f6
chrislangley666 <chrislangley666@hotmail.co.uk>
2015-03-26 17:28:22 +0000

    TEST

--- include/house-card.php
+++ include/house-card.php
@@ -3,6 +3,9 @@
                 <div class="house-image">
                    <img style="width: 290px; height: 230px; float:left;" alt="Oops the image is missing!" src="<?php echo $row["house_photo_path"]; ?> ">   
                 
+                <div class="more-info">
+                    <a href="house-card-info.php?id= <?php echo $row ["id"]; ?>">  
+                </div>
     
                 <div class="user-id-box">
                     <p style="display:inline;">User ID: <?php echo $row["user_id"]; ?></p>
