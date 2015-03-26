fadfd16e1de2201cf09ce8c59c0f3004307bc65c
chrislangley666 <chrislangley666@hotmail.co.uk>
2015-03-26 17:06:32 +0000

    TEST

--- include/house-card.php
+++ include/house-card.php
@@ -1,9 +1,9 @@
 <div class="house-card">
     
                 <div class="house-image">
-                   <img style="display: inline-block;"src="../public/images/Search-50.png"> <?php echo $row["house_photo"]; ?>   
+                   <img src="../public/images/Search-50.png"> <?php echo $row["house_photo"]; ?>   
                 
-                </div>
+    
                 <div class="user-id-box">
                     <p style="display:inline;">User ID: <?php echo $row["user_id"]; ?></p>
                     <p style="display:inline;"> Date added:<?php echo $row["date_added"]; ?></p>
@@ -29,6 +29,6 @@
                         <p>Bedroom Number: <?php echo $row["house_bedroomnumber"]; ?></p>
                 </div>
                  
-                
+                </div>
                 
 </div>
