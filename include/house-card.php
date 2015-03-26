1a9389a74274b776068b740835d21f9f5f845632
chrislangley666 <chrislangley666@hotmail.co.uk>
2015-03-26 17:02:24 +0000

    TEST

--- include/house-card.php
+++ include/house-card.php
@@ -2,32 +2,33 @@
     
                 <div class="house-image">
                    <img style="display: inline-block;"src="../public/images/Search-50.png"> <?php echo $row["house_photo"]; ?>   
+                    
+                                    
+                </div>
                 
-    
-                    <p style="display:inline; margin-left: 250px">User ID: <?php echo $row["user_id"]; ?></p>
-                    <p style="display:inline;"> Date added:<?php echo $row["date_added"]; ?></p>
+                <div class="house-price">
+            <p style="display:inline; margin-left: 250px">User ID: <?php echo $row["user_id"]; ?></p>
+            <p style="display:inline;"> Date added:<?php echo $row["date_added"]; ?></p>
             
     
-                <div class="house-price">
-                        <?php echo $row ["house_price"]; ?>
-                </div>
-
-                <div class="house-location">
-                        <?php echo $row["house_location"]; ?>
-                </div>
-        
-                <div class="house-title">
-                        <?php echo $row["house_title"]; ?>
-                </div>
-
-                <div class="house-type">
-                        <p>House Type: <?php echo $row["house_type"]; ?></p>
-                </div>
                 
-                <div class="house-bedroomnumber">
+                        <?php echo $row ["house_price"]; ?>
+                
+
+    
+                        <?php echo $row["house_location"]; ?>
+            
+        
+             
+                        <?php echo $row["house_title"]; ?>
+                
+
+              
+                        <p>House Type: <?php echo $row["house_type"]; ?></p>
+               
+                
+               
                         <p>Bedroom Number: <?php echo $row["house_bedroomnumber"]; ?></p>
-                </div>
+               
                  
                 </div>
-                
-</div>
