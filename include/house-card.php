32516c5e2c3fb252264999d67149c134d5bd4ac8
chrislangley666 <chrislangley666@hotmail.co.uk>
2015-03-27 14:01:24 +0000

    TEST

--- include/house-card.php
+++ include/house-card.php
@@ -5,7 +5,7 @@
                    <img style="width: 290px; height: 230px; float:left;" alt="Oops the image is missing!" src="<?php echo $row["house_photo_path"]; ?> ">   
     
                 <div class="house-price">
-                £ <?php echo $row ["house_price"]; ?>
+                &nbsp;£<?php echo $row ["house_price"]; ?>
                 </div>
 
                 <div class="house-location">
