bfaf6b988b925620933a2b63699efda63effd267
chrislangley666 <chrislangley666@hotmail.co.uk>
2015-03-27 14:33:22 +0000

    TEST

--- include/search-dropdown.php
+++ include/search-dropdown.php
@@ -17,5 +17,5 @@
     <select name="sort-by">
         <option value="new">New</option>
         <option value="old">Old</option>
-    </select><input name="search" value="Find!" type="submit" a href="#results"> 
+    </select><a href="#results"><input name="search" value="Find!" type="submit"> 
 </form>
