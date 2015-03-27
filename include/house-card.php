1182f7bae0757e56c3a116f4acfc900e5c0339b5
chrislangley666 <chrislangley666@hotmail.co.uk>
2015-03-27 13:41:59 +0000

    House card changes (Display)

--- include/house-card.php
+++ include/house-card.php
@@ -5,8 +5,8 @@
                    <img style="width: 290px; height: 230px; float:left;" alt="Oops the image is missing!" src="<?php echo $row["house_photo_path"]; ?> ">   
                 
                 <div class="user-id-box">
-                    <p style="display:inline;">User ID: <?php echo $row["user_id"]; ?></p>
-                    <p style="display:inline;"> Date added:<?php echo $row["date_added"]; ?></p>
+                   User ID: <?php echo $row["user_id"]; ?>
+                   Date added:<?php echo $row["date_added"]; ?>
                 </div>
     
                 <div class="house-price">
@@ -22,11 +22,11 @@
                 </div>
 
                 <div class="house-type">
-                        <p>House Type: <?php echo $row["house_type"]; ?></p>
+                        House Type: <?php echo $row["house_type"]; ?>
                 </div>
                 
                 <div class="house-bedroomnumber">
-                        <p>Bedroom Number: <?php echo $row["house_bedroomnumber"]; ?></p>
+                        Bedroom Number: <?php echo $row["house_bedroomnumber"]; ?>
                 </div>
                  
                 </div>
