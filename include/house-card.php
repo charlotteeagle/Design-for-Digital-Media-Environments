385ed7a2c0ca4bc0d5d08165f4641f34792c556c
David <thedavidwatts@gmail.com>
2015-04-13 17:12:21 +0100

    £ added to house card

--- include/house-card.php
+++ include/house-card.php
@@ -5,7 +5,7 @@
                    <img style="width: 290px; height: 230px; float:left;" alt="Oops the image is missing!" src="<?php echo $row["house_photo_path"]; ?> ">   
     
                 <div class="house-price">
-                <?php echo $row ["house_price"]; ?></div>
+                 £ &nbsp;<?php echo $row ["house_price"]; ?></div>
 
                 <div class="house-location">
                         <?php echo $row["house_location"]; ?>
