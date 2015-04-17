964a8a44dccbad4cc25b55074d7ddce6d9ecccf3
charlotteeagle <i7676522@bournemouth.ac.uk>
2015-04-17 10:37:18 +0100

    About and Help pages

--- public/about.php
+++ public/about.php
@@ -6,8 +6,56 @@
 ?>
 
 <div class="main">
-    <div class="background-position" > 
-    </div> 
+    <div class="about">
+        <h2 align="center" style= "font-size: 3em"> About Us </h2>
+
+
+		<div class="col-3">
+			<div class="spacing">
+				<div id="f1_container">
+		            <div id="f1_card" class="shadow">
+		                <div class="front face">
+		                    <img class="img-width" src="images/david.png">
+		                </div>
+
+		                <div class="back face center">
+		                    <p style= "font-size: 1.5em">I am from Bournemouth and currently studying for a BA (Hons) degree in Digital Media Design at Bournemouth University. Before this I studied Media and Journalism at The Bournemouth and Poole College. Some of my aspirations include, app development, creating projects using the Arduino/Raspberry Pi and web design. I am also keen to get started on my latest assignment creating a website using HTML, PHP and MySQL. Some other areas I am interested in are augmented reality, virtual reality and virtual retinal displays, in particular Oculus Rift, Avegant Glyph and Microsoft HoloLens. As far as interests such as sport go, I am an avid fan of Formula 1. The picture I have chosen to include with this bio was taken of me learning to surf when I was travelling down the east coast of Australia. While in Australia I also enjoyed doing various other activities such as bungee jumping, white water rafting, hang gliding, sightseeing and of course meeting lots of new people.</p>
+		                </div>
+		            </div>
+				</div>
+	        </div>
+		</div>
+
+		<div class="col-3">
+			<div class="spacing">
+		        <div id="f2_container">
+		            <div id="f2_card" class="shadow">
+		                <div class="front face">
+		                    <img class="img-width" src="images/chris.png">
+		                </div>
+
+		                <div class="back face center2">
+		                    <p style= "font-size: 1.5em">Hi, I’m Chris Langley. Currently, I’m studying Digital Media Design at Bournemouth University. I’m a passionate person and have a number of hobbies, such as keeping fit and feeling good. I’m also interested in the technological age we’re in and how it’s changing our world as we know it - exciting times! Technology has always been in my life in some form, therefore I’ve always been interested in how things work and what potential new technologies have. This course has certainly widened my perspective in numerous ways in regards to digital media and hopefully I will find a specific subject that will inspire me to pursue an exciting, long-term career.</p>
+		                </div>
+		            </div>
+		        </div>
+	        </div>
+		</div>
+
+		<div class="col-3">
+			<div class="spacing">
+		        <div id="f3_container">
+		            <div id="f3_card" class="shadow">
+		                <div class="front face">
+		                    <img class="img-width" src="images/charlotte.png">
+		                </div>
+		                <div class="back face center3">
+		                    <p style="font-size:1.5em">Hi, I'm Charlotte Eagle. I'm in my first year of studying Digital Media design at Bournemouth University. I am particularly passionate for art and design, and have an avid interest in fashion and photography. I have a number of hobbies outside of my studies such as, performing arts and music. I find the technological shifts in an ever changing world fascinating and have thus chosen to study digital media to enhance my knowledge and insight into this innovative culture. I look forward to learning the different aspects this course entails whilst bettering my skills.<p>
+		                </div>
+		            </div>
+		        </div>
+	        </div>
+		</div>
 </div>
 
 <?php include_once("../include/templates/footer.php"); ?> 
--- public/help.php
+++ public/help.php
@@ -5,9 +5,57 @@
     include_once("../include/templates/menuheader.php");
 ?>
 
+<meta name="viewport" content="initial-scale=1, maximum-scale=1">
+
+	
+	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Abeezee:400|Open+Sans:400,600,700|Source+Sans+Pro:400,600">
+	<link rel="stylesheet" type="text/css" href="demo.css">
+
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	<script type="text/javascript" src="../public/js/help.js"></script>
+</head>
+<body>
+    
+    <div class="help">    
 <div class="main">
-    <div class="background-position" >            
-    </div> 
+    <h1 align= "center" style="font-size: 3em">Bournemouth Homes Help</h1>
+                <img class="img-width" src="images/View_from_balloon,_facing_Swanage_-_geograph.org.uk_-_317087.jpg">
+
+
+	<div class="accordion">
+			<div class="accordion-section">
+				<a class="accordion-section-title" href="#accordion-1">What Is Bournemouth Homes</a>
+				<div id="accordion-1" class="accordion-section-content">
+					<p style="font-size: 1.8em">Bournemouth Homes is an easy to use and efficient web application which allows you to search houses for sale in the Bournemouth area, showing you the locations, prices and house images in order for you to find your perfect home. As well as displaying search results you are able to post your own properties through entering the appropriate information, after signing up. </p>
+				</div>
+			</div>
+
+			<div class="accordion-section">
+				<a class="accordion-section-title" href="#accordion-2">How To Search</a>
+				<div id="accordion-2" class="accordion-section-content">
+					<p style="font-size: 1.8em">There is no need to sign up to Bournemouth Homes in order to browse through the house listings. On the home page you are able to filter the house results by choosing the location you desire from the drop down menu.  You are then given the option the sort your results by showing the newest or oldest listings first.  Clicking on find will reveal the results below. </p>
+				</div>
+			</div>
+
+			<div class="accordion-section">
+				<a class="accordion-section-title" href="#accordion-3">How To Sign Up / Log In</a>
+				<div id="accordion-3" class="accordion-section-content">
+					<p style="font-size: 1.8em">If you are interested in posting your own houses for sale you will need to sign up to Bournemouth Homes. This quick and easy process entails you filling out an account form which asks for your name, email and password. You can sign up by clicking on the 'sign up' button, which can be found in the top navigation bar. Once you have signed up, you can easily log in the next time you return or time out by clicking on 'login' on the top right hand corner of the home page. After entering your username and password correctly you will be greeted back. From then on you may submit your properties to the listings.</p>
+				</div>
+			</div>
+        <div class="accordion-section">
+				<a class="accordion-section-title" href="#accordion-4">Track The Web Applications Development</a>
+				<div id="accordion-4" class="accordion-section-content">
+					<p style="font-size: 1.8em">Interested in the development and creation of this property web application? Click on the wiki button placed in the top navigation bar to see our groups research and thoughts during the planning process for our web application task. 
+To see the group members individual thoughts and ideas towards this task, click on the name below to see the linked members blog...</p>
+                    <li><a href= " http://chrislangley666.wordpress.com" style="font-size:1.8em">Chris's Blog</a></li>
+                    <li><a href="http://davidwattsdmd.wordpress.com" style="font-size:1.8em">David's Blog</a></li>
+                    <li><a href="http://charlotteeagle.wordpress.com" style="font-size:1.8em">Charlotte's Blog</a></li>
+				</div>
+			</div>
+		</div>
+
+	</div>     
 </div>
 
 <!-- Begin Footer -->
